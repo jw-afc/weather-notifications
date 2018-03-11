2b8203352a8cb4839da0521a1ca2608555c618b4
jonathan withers <jonathan_withers@hotmail.com>
2018-03-11 12:27:45 +0000

    Add high/low wind condition handling to rationalise the alerts sent

--- WeatherNotifications/Scheduler.cs
+++ WeatherNotifications/Scheduler.cs
@@ -18,16 +18,16 @@ namespace WeatherNotifications
 	{
 		private Timer _timer;
 		private DateTime _executionDate;
-		
+
 		private string _alertSubject = "➹ Wind Speed Notifications";
 		private int _maximumWindSpeed = int.Parse(ConfigurationManager.AppSettings["MAXIMUM_WIND_SPEED_IN_KPH"]);
 		private string _postcode = ConfigurationManager.AppSettings["POSTCODE"];
-		
+
 		private string WeatherUrl => $"{ConfigurationManager.AppSettings["WEATHER_URL_BASE"]}/{_postcode.Split(' ')[0]}".ToLower();
 
 		private TimeZoneInfo _timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
-		private IDictionary<string, decimal> _windConditions = new Dictionary<string, decimal>();
-				
+		private IDictionary<string, WindCondition> _windConditions = new Dictionary<string, WindCondition>();
+
 		private object _lock = new object();
 
 		public static Scheduler Instance { get; } = new Scheduler();
@@ -84,10 +84,15 @@ namespace WeatherNotifications
 
 		private void Reset()
 		{
-			_executionDate = TimeZoneInfo.ConvertTime(DateTime.Now, _timeZoneInfo);
-			_windConditions = new Dictionary<string, decimal>();
+			_executionDate = TimeZoneInfo.ConvertTime(DateTime.Now.AddDays(2), _timeZoneInfo);
+			
+			_windConditions.Remove("current");
+			foreach (var windCondition in _windConditions.Where(p => DateTime.Parse(p.Key) < _executionDate.Date).ToList())
+			{
+				_windConditions.Remove(windCondition.Key);
+			}
 		}
-		
+
 		private void AnalyseWeather(Weather weather)
 		{
 			AnalyseCurrent(weather.Current);
@@ -97,7 +102,7 @@ namespace WeatherNotifications
 		private void AnalyseCurrent(Current current)
 		{
 			var windCondition = AnalyseWind(current?.Wind, "current");
-			if (windCondition.Alert)
+			if (windCondition.Send)
 			{
 				var sb = new StringBuilder();
 				sb.Append($"<div><h3>Local Weather - {_postcode}</h3></div>");
@@ -125,7 +130,7 @@ namespace WeatherNotifications
 
 				var windCondition = AnalyseForecastDay(day, date.ToString("dd/MM/yyyy"));
 
-				if (windCondition.Alert)
+				if (windCondition.Send)
 				{
 					sb.Append($"<br /> &nbsp;- {date.ToString("dd/MM/yyyy")} - {GetWindConditions(day.Wind)}{GetChangeIndicator(windCondition.Change, day.Wind.Unit)}");
 					foreach (var timeframe in day.Timeframes.Where(p => p.Wind.Speed >= _maximumWindSpeed))
@@ -135,7 +140,7 @@ namespace WeatherNotifications
 					}
 					alert = true;
 				}
-				
+
 				if (alert)
 				{
 					sb.Append($"<br /><br /><a href=\"{WeatherUrl}\">{WeatherUrl}</a>");
@@ -143,34 +148,50 @@ namespace WeatherNotifications
 				}
 			}
 		}
-				
-		private WindCondition AnalyseForecastDay(ForecastDay forecastDay, string descriptor)
+
+		private WindAlert AnalyseForecastDay(ForecastDay forecastDay, string descriptor)
 		{
-			return forecastDay != null ? AnalyseWind(forecastDay.Wind, descriptor) : new WindCondition(false, 0);
+			return forecastDay != null ? AnalyseWind(forecastDay.Wind, descriptor) : new WindAlert(false, 0);
 		}
 
-		private WindCondition AnalyseWind(Wind wind, string descriptor)
+		private WindAlert AnalyseWind(Wind wind, string descriptor)
 		{
 			if (wind != null)
 			{
-				if (_windConditions.ContainsKey(descriptor))
-				{
-					var previous = _windConditions[descriptor];
-					if (wind.Speed != previous)
-					{
-						_windConditions[descriptor] = wind.Speed;
-						return new WindCondition(wind.Speed > _maximumWindSpeed, wind.Speed - previous);
-					}
-				}
+				if (_windConditions.ContainsKey(descriptor)) return AnalyseWindConditionHistory(wind, descriptor);
 				else
 				{
-					_windConditions.Add(new KeyValuePair<string, decimal>(descriptor, wind.Speed));
-					return new WindCondition(wind.Speed > _maximumWindSpeed, 0);
+					_windConditions.Add(new KeyValuePair<string, WindCondition>(descriptor, new WindCondition(wind.Speed)));
+					return GetWindAlert(wind);
 				}
 			}
-			return new WindCondition(false, 0);
+			return GetWindAlert();
 		}
-		
+
+		private WindAlert AnalyseWindConditionHistory(Wind wind, string descriptor)
+		{
+			var previous = _windConditions[descriptor];
+
+			if (wind.Speed > previous.High)
+				return GetWindAlert(wind, wind.Speed - previous.High, previous);
+
+			if (wind.Speed < previous.Low)
+				return GetWindAlert(wind, wind.Speed - previous.Low, previous);
+
+			return GetWindAlert();
+		}
+
+
+		private WindAlert GetWindAlert(Wind wind = null, decimal change = 0, WindCondition previous = null)
+		{
+			if (wind != null)
+			{
+				previous?.Update(wind.Speed);
+				return new WindAlert(wind.Speed > _maximumWindSpeed, change);
+			}
+			return new WindAlert();
+		}
+
 		private string GetChangeIndicator(decimal change, string unit)
 		{
 			if (change == 0) return string.Empty;
@@ -218,16 +239,33 @@ namespace WeatherNotifications
 			_timer?.Dispose();
 		}
 
-		private class WindCondition
+		private class WindAlert
 		{
-			public bool Alert { get; set; }
+			public bool Send { get; set; }
 			public decimal Change { get; set; }
 
-			public WindCondition(bool alert, decimal change)
+			public WindAlert(bool send = false, decimal change = 0)
 			{
-				Alert = alert;
+				Send = send;
 				Change = change;
 			}
 		}
-	}	
+
+		private class WindCondition
+		{
+			public decimal High { get; set; }
+			public decimal Low { get; set; }
+
+			public WindCondition(decimal speed)
+			{
+				High = Low = speed;
+			}
+
+			public void Update(decimal speed)
+			{
+				High = speed > High ? speed : High;
+				Low = speed < Low ? speed : Low;
+			}
+		}
+	}
 }
